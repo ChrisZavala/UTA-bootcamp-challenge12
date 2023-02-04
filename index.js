a5b5e3603f20cc742e346ac505d291bb15bbce16
Chris Zavala <frappejcat@gmail.com>
2023-02-03 18:26:31 -0600

    added the viewAllemployees function.

--- index.js
+++ index.js
@@ -322,6 +322,28 @@ async function viewAllDepartments() {
         console.log(err);
     } 
 }
+//viewAllEmployees()
+async function viewAllEmployees() {
+    try {   
+        const rows = await db.query(
+            `SELECT employee.id, employee.first_name AS "first name", employee.last_name
+                AS "last name", role.title, department.name AS department, role.salary,
+                concat(manager.first_name, " ", manager.last_name) AS manager
+                FROM employee
+                LEFT JOIN role
+                ON employee.role_id = role.id
+                LEFT JOIN department
+                ON role.department_id = department.id
+                LEFT JOIN employee manager
+                ON manager.id = employee.manager_id`);
+        console.table(rows);
+        return rows;
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
 
 
 
